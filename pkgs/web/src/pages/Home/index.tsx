dfc81d8cbc9bda3aced1be07df30cb9d1df5dc2c
Reorx <novoreorx@gmail.com>
2023-01-17 02:39:03 +0800

    add sample link

--- pkgs/web/src/pages/Home/index.tsx
+++ pkgs/web/src/pages/Home/index.tsx
@@ -2,9 +2,9 @@ import { useRef } from 'react';
 
 import { Icon } from '@iconify/react';
 import {
-  Container, Global, TextInput, Stack, ActionIcon, useMantineTheme,
+  Text, Container, Global, TextInput, Stack, ActionIcon, useMantineTheme,
 } from '@mantine/core';
-import { createSearchParams, useNavigate } from 'react-router-dom';
+import { createSearchParams, Link, useNavigate } from 'react-router-dom';
 
 import { HomeFooter } from './Footer';
 import { HomeHeader } from './Header';
@@ -60,10 +60,13 @@ export function HomePage() {
                 <Icon icon="tabler:arrow-right" />
               </ActionIcon>
             }
-            placeholder="Paste Wikipedia URL here"
+            placeholder="Paste the Wikipedia URL here"
             rightSectionWidth={42}
             onKeyDown={enterAction}
           />
+        </Container>
+        <Container>
+          <Text align='center' mt={16}>Or open a <Link to="/extractor?url=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FFeudalism">sample page</Link></Text>
         </Container>
       </Container>
       <HomeFooter/>
