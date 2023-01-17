9516822a60a7814d1a5e5301c1f4381eeb26fb13
Reorx <novoreorx@gmail.com>
2023-01-18 01:28:39 +0800

    reduce repetitions in site.js

--- pkgs/web/site.js
+++ pkgs/web/site.js
@@ -5,66 +5,73 @@ const description = 'Extract substance from the web'
 const url = 'https://substance.reorx.com'
 const image_url = url + '/cover.png'
 
-module.exports = {
-  favicon: './public/favicon.svg',
-  title,
-  meta: {
-    'title': {
+const meta_tags = [
+    {
       name: 'title',
       content: title,
     },
-    'description': {
+    {
       name: 'description',
       content: description,
     },
 
     // opengraph
-    'og:title': {
+    {
       property: 'og:title',
       content: title,
     },
-    'og:description': {
+    {
       property: 'og:description',
       content: description,
     },
-    'og:url': {
+    {
       property: 'og:url',
       content: url,
     },
-    'og:image': {
+    {
       property: 'og:image',
       content: image_url,
     },
-    'og:type': {
+    {
       property: 'og:type',
       content: 'website'
     },
 
     // twitter
-    'twitter:card': {
+    {
       name: 'twitter:card',
       property: 'twitter:card',
       content: 'summary_large_image',
     },
-    'twitter:image': {
+    {
       name: 'twitter:image',
       property: 'twitter:image',
       content: image_url,
     },
-    'twitter:creator': {
+    {
       name: 'twitter:creator',
       property: 'twitter:creator',
       content: '@novoreorx',
     },
-    'twitter:title': {
+    {
       name: 'twitter:title',
       property: 'twitter:title',
       content: title,
     },
-    'twitter:description': {
+    {
       name: 'twitter:description',
       property: 'twitter:description',
       content: description,
     },
-  }
+]
+
+const meta = {}
+meta_tags.forEach(o => {
+  meta[o.name || o.property] = o
+})
+
+module.exports = {
+  favicon: './public/favicon.svg',
+  title,
+  meta,
 }
